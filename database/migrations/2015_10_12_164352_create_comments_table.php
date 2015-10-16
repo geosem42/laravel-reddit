1524c6b15baf817b6d09cab756136e67c86a5388
Halnex <halnex@yandex.com>
2015-10-17 00:15:22 +0300

    comments migration

---
+++ database/migrations/2015_10_12_164352_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 32)->nullable();
+            $table->string('email', 32)->nullable();
+            $table->string('comment', 400)->unique(); // made unique so as to avoid duplicates
+            $table->integer('user_id')->default(0);
+            $table->integer('parent_id')->default(0); // links to parent comment
+            $table->string('parents')->nullable();
+            $table->softDeletes(); // this adds 'deleted_at' column in the Users table
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
