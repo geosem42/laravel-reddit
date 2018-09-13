c10e498483e1ee87e086dc890507fd6cf895ac52
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-12 22:39:44 -0400

    update

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
+            'database'  => env('DB_DATABASE', 'irt'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', 'anyPassword'),
             'charset'   => 'utf8',
