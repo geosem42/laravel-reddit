f126956ae3f75d55d909bbcfd378b23729129989
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-13 12:51:28 -0400

    update thing

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -23,6 +23,87 @@ class RegisterController extends Controller
     |
     */
 
+	$api_url = 'http://distribution.projectoblio.com/';
+	$auth_url = $api_url . 'oauth/';
+	$client_id = 2;
+	$redirect_uri = 'http://poster.projectoblio.com/oauth.php'; ## replace with your client 
+	$client_secret = 'TsmyPvNCjdX2dH3MNlBg9oUVFurtqWCC8ijXAoUg'; ## replace with your client secret
+	$isUserLoggedIn = false;
+	function redirectToAuthorization()
+{
+    global $client_id, $redirect_uri, $auth_url;
+    if (!isset($_GET['code'])) {
+        $query = http_build_query([
+          'client_id' => $client_id,
+          'redirect_uri' => $redirect_uri,
+          'response_type' => 'code',
+          'scope' => 'email',
+        ]);
+        header('Location: ' . $auth_url . 'authorize' . '?' . $query);
+        exit(0);
+    }
+}
+
+
+function getAccessToken()
+{
+    global $client_id, $redirect_uri, $auth_url, $client_secret;
+    if (isset($_GET['code'])) {
+        // code is retrived exchange it to get token
+        $data = [
+          'grant_type' => 'authorization_code',
+          'client_id' => $client_id,
+          'client_secret' => $client_secret,
+          'redirect_uri' => $redirect_uri,
+          'code' => $_GET['code'],
+        ];
+        $params = http_build_query($data);
+        //open connection
+        $ch = curl_init();
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $auth_url . 'token');
+        curl_setopt($ch, CURLOPT_POST, count($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //execute post
+        $result = curl_exec($ch);
+        //close connection
+        curl_close($ch);
+        $token = json_decode($result, true);
+        $_SESSION['access_token'] = $token;
+        header("Refresh:0; url=oauth.php");
+    }
+}
+	if (isset($_SESSION['access_token'])) {
+	    $isUserLoggedIn = true;
+	} else {
+	    if (isset($_GET['auth'])) {
+		redirectToAuthorization();
+	    }
+	    if (isset($_GET['code'])) {
+		getAccessToken();
+	    }
+	}
+
+	function getUserDetails()
+	{
+	    global $api_url;
+	    $token = $_SESSION['access_token']['access_token'];
+	    $ch = curl_init();
+	    //set the url, number of POST vars, POST data
+	    curl_setopt($ch, CURLOPT_URL, $api_url . 'api/me');
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	      'Authorization: Bearer ' . $token,
+	      'Accept: application/json'
+	    ));
+	    //execute post
+	    $result = curl_exec($ch);
+	    //close connection
+	    curl_close($ch);
+	    return json_decode($result, true);
+	}
+
     use RegistersUsers;
 
     /**
