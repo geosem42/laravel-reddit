488aa00dc1f66c94c9723983fc3aaee9e1c27c89
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-12 22:20:18 -0400

    update

--- localUpdate.sh
+++ localUpdate.sh
@@ -3,3 +3,7 @@ git fetch all;
 git reset --hard origin/master;
 git pull;
 rsync -ravv ./laravel/* /var/www/laravel/
+cd /var/www/; 
+chown -R :www-data /var/www/laravel; 
+chmod -R 775 /var/www/laravel/storage; 
+chmod -R 775 /var/www/laravel/bootstrap/cache; 
