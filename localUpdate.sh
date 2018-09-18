d9765d354981d663a93132e7670363766e0f6be7
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-18 16:13:50 -0400

    update

--- localUpdate.sh
+++ localUpdate.sh
@@ -4,33 +4,35 @@ if [ $# -eq 0 ] ; then
     exit 1
 fi
 
-branch=$1;
-git fetch;
-#git reset --hard origin/master;
-git pull origin $branch;
-rsync -ravv ./laravel/* /var/www/laravel/
-cp ./laravel/.* /var/www/laravel/
-cd /var/www/; 
-chown -R :www-data /var/www/laravel; 
-chmod -R 775 /var/www/laravel/storage; 
-chmod -R 775 /var/www/laravel/bootstrap/cache; 
-cd laravel;
-rm -rf vendor;
-composer dump-autoload;
-php artisan clear-compiled; 
-rm -rf bootstrap/cache/packages.php;
-rm -rf bootstrap/cache/services.php;
-composer install --no-scripts
-composer update; 
-php artisan key:generate;
-chown -R :www-data /var/www/laravel; 
-chmod -R 775 /var/www/laravel/storage; 
-chmod -R 775 /var/www/laravel/bootstrap/cache; 
+if [ $# -eq 2 ] || [ $# -eq 3 ] ; then 
+	branch=$1;
+	git fetch;
+	#git reset --hard origin/master;
+	git pull origin $branch;
+	rsync -ravv ./laravel/* /var/www/laravel/
+	cp ./laravel/.* /var/www/laravel/
+	cd /var/www/; 
+	chown -R :www-data /var/www/laravel; 
+	chmod -R 775 /var/www/laravel/storage; 
+	chmod -R 775 /var/www/laravel/bootstrap/cache; 
+	cd laravel;
+	rm -rf vendor;
+	composer dump-autoload;
+	php artisan clear-compiled; 
+	rm -rf bootstrap/cache/packages.php;
+	rm -rf bootstrap/cache/services.php;
+	composer install --no-scripts
+	composer update; 
+	php artisan key:generate;
+	chown -R :www-data /var/www/laravel; 
+	chmod -R 775 /var/www/laravel/storage; 
+	chmod -R 775 /var/www/laravel/bootstrap/cache; 
 
-if [ $# -eq 2 ] ; then
-    echo 'Updating mysql'
-    mysql -uroot -panyPassword -e "drop database irt;" 
-    mysql -uroot -panyPassword -e "create database irt;"
+	if [ $# -eq 3 ] ; then
+    		echo 'Updating mysql'
+   		mysql -uroot -panyPassword -e "drop database irt;" 
+    		mysql -uroot -panyPassword -e "create database irt;"
+	fi
 fi
 
 php artisan migrate; 
