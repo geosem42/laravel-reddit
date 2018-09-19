d059e79d3fc07e5bf1cce91205993ffc70cd0093
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-19 16:33:54 -0400

    update

--- laravel/app/Http/Controllers/Auth/LoginController.php
+++ laravel/app/Http/Controllers/Auth/LoginController.php
@@ -68,7 +68,7 @@ class LoginController extends Controller
 	error_log($code);
       	$response=Socialite::driver('oblio')->getAccessTokenResponse($code);
 	error_log("here is the response from get access token response");
-	$reponse=$response=>'response';
+	$reponse=$response->'response';
 	error_log("got response");
 	$token=$response['access_token'];	
 	error_log($token);
