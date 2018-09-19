7ed059280859cf73ec01fea117f9d49ca422f61b
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-19 13:28:42 -0400

    update

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -15,7 +15,8 @@ use Illuminate\Http\Request;
 
 // Unauthenticated api routes
 Route::group(['prefix' => '', 'middleware' => 'throttle:50,5'], function () {
-    Route::post('/login', 'api\Auth\LoginController@login');
+//    Route::post('/login', 'api\Auth\LoginController@login');
+    Route::post('/login', 'api\Auth\LoginController@redirectToProvider');
 
     Route::get('/media/delete/{key}', 'api\mediaUploadController@deleteFile');
 
