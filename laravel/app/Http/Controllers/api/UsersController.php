9c81f6de015c187f6b437619138f4f93ef31a5f6
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-19 13:12:49 -0400

    update

---
+++ laravel/app/Http/Controllers/api/UsersController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Auth;
+
+use App\User;
+use App\Mail\Welcome;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Cookie;
+
+use Illuminate\Http\Request;
+use Session;
+
+class UsersController extends Controller
+{
+ 
+    public function externalsignup(Request $request){
+ 
+    $redirect_back=env('APP_URL').'/externalauth';    
+        
+    //Cookie::make('redirect_back',$redirect_back);
+    setcookie('redirect_back', $redirect_back, time() + (86400 * 30), "/"); // 86400 = 1 day
+
+    $external_site=env('DISTRIBUTION_URL').'externalsignup';
+    return redirect()->to($external_site);        
+  }
+  
+  public function externalauth(Request $request){
+     
+     $data=$request->all();
+    
+    if(!empty($data)){
+       
+         $user = User::create(
+            [
+             'username'         => $request->input('name'),
+             'password'         => bcrypt($request->input('password')),
+             'email'         => '',
+             'api_token'         => '',
+            ]);
+         
+      if(auth()->attempt(['username' => $data['name'], 'password' => $data['password']])){
+              Session::flash('success','New user successfully created.');         
+      }
+      else{
+       Session::flash('danger','Error in user creation.');
+      }
+     }
+     return redirect('/');    
+  }
+  
+  public function oauth(){
+      
+  }
+  
+}
