54c96e29310191e987de3c777c887f5d5d489053
jignesh.j.patel <jignesh.j.patel@doyenhub.com>
2018-09-27 12:25:16 +0530

    create

--- laravel/config/reddit-api.php
+++ laravel/config/reddit-api.php
@@ -4,10 +4,10 @@ return [
     'endpoint_standard' => 'https://www.reddit.com',
     'endpoint_oauth' => 'https://oauth.reddit.com',
     
-    'username' => 'pateljignesh142',
-    'password' => 'vision142',
-    'app_id' => 'WeSVT6IuGuMCSQ',
-    'app_secret' => 'xVK9O-5O37cboEAMHyay0rxqjqE',
+    'username' => env('REDDIT_USERNAME'),
+    'password' => env('REDDIT_PASSWORD'),
+    'app_id' => env('REDDIT_APP_ID'),
+    'app_secret' => env('REDDIT_APP_SECRET'),
     
     'response_format' => 'STD', // STD | ARRAY
     
