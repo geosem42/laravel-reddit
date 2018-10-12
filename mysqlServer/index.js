76ed847a1debf3d53f79fa859e12e215ba5b3428
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-10-11 22:34:04 -0400

    update

--- mysqlServer/index.js
+++ mysqlServer/index.js
@@ -1,9 +1,12 @@
 'use strict'
+// Only keep a certain number of files
 
 const Tail = require('tail').Tail;
 var tail = new Tail("/var/lib/mysql/general.log");
 tail.watch()
 tail.on("line", data => {
-  console.log(data.split("/"));
+	command=data.split("\t")[2];
+	
+  console.log(command);
 	
 });
